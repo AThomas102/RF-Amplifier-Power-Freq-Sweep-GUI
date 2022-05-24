f09ceeac5bdc3d0105cefc4a07548e94efe8969b
Andrew Thomas <77105338+AThomas102@users.noreply.github.com>
2022-05-24 13:10:28 +0100

    Update RF Amplifier GUI.py
    Added some more rf  and dc instrument functions

--- RF Amplifier GUI.py
+++ RF Amplifier GUI.py
@@ -5,8 +5,10 @@
 from contextlib import nullcontext
 from msilib import Table
 from msilib.text import tables
+from multiprocessing.connection import wait
 from sqlite3 import connect
 import PySimpleGUI as sg
+from numpy import arange
 import pyvisa as visa
 import os
 import math
@@ -19,8 +21,8 @@ key_list1 = [('_Pa' + str(c) + '_') for c in range(2)] + [('_Pb' + str(c) + '_')
 
 
 newinstr = True
-instr_data = [["DC Supply 1","GPIB0::2::INSTR"], ["DC Supply 2","GPIB0::3::INSTR"]]
-instr_names = ["DC Supply 1","DC Supply 2"]
+instr_data = [["DC Supply 1","GPIB0::2::INSTR"], ["DC Supply 2","GPIB0::3::INSTR"],["DC Supply 3","GPIB0::10::INSTR"],["DC Supply 4","GPIB0::11::INSTR"]]
+instr_names = ["DC Supply 1","DC Supply 2","DC Supply 3","DC Supply 4"]
 lib_select_keys = ["_PO1_", "_PO2_", "_FO1_"]
 lib_change_keys = ["_PLIB1_", "_PLIB2_", "_FLIB1_"]
 """
@@ -29,28 +31,57 @@ def instr_add(value1, value2):
     data.append()
 """
 
-def instr_open():                  # open and list all instrument names and check whether they are active
-    global list
-    for i in range(1, len(list)):
-        print('--------- Instrument ' + str(i) + ' -----------')
-        name = list[i]
-        active = [False for x in range(len(list))]
+def instr_open():                  # open and list all instrument names on program start and check whether they are active
+    for i in range(0, len(instr_list)):
+        print('--------- Instrument ' + str(i+1) + ' -----------')
+        name = instr_list[i]
+        active = [False for x in range(len(instr_list))]
         try:
             resource = rm.open_resource(name)
             instr = resource.query('*IDN?')   
             instr = instr.strip()             
             print(instr)
-            address = resource.query('ADDRESS?')
-            address = address.strip()
-            print('address: ' + address)
+            try:
+                address = resource.query('ADDRESS?')
+                address = address.strip()
+                print('address: ' + address)
+            except Exception as e:
+                print('address command not recognised')
             active[i] = True
             print('connected: ' + str(active[i]))
 
         except Exception as e:
             print(type(e))
-            print(name + ' resource not active')
+            print(name + ' resource not active or *IDN? command not recognised')
             print('connected: ' + str(active[i]))
     return active
+
+def option_menu_update(window, values, libsl, libck, instr_names, instr_data):  # updates the current device from the one selected in the option menu
+    for i in range(len(libsl)):
+        print('i=' + str(i))
+        k = values[libsl[i]]                                        # gives current frequency tab option value
+        print('=> option menu ' + libsl[i] + ' = ' + k) 
+        try:                                                
+            index = instr_names.index(k)
+            print(index)
+            try:
+                sel_address = instr_data[index][1]                      # finds address for selected instrument
+            except Exception as e:
+                print(type(e))
+                print('no device found with this GPIB address')
+
+            cur_lib_name = lib_select(sel_address)
+            print(cur_lib_name)
+            window[libck[i]].update(cur_lib_name)                  # shows current active library
+            if cur_lib_name == 'none':
+                window[libck[i]].update(text_color='red')
+            else:
+                window[libck[i]].update(text_color='LightGreen')
+            print('library text updated')
+        except Exception as e:
+            print(type(e))
+            print('UPDATE key = ' + libsl[i] + ' failed')
+
 
 def lib_select(GPIB_address):               # auto selects the library to use
     try:
@@ -62,35 +93,100 @@ def lib_select(GPIB_address):               # auto selects the library to use
         print(type(e))
         print('-device library unavailable-')
         instr = 'none' 
-    if 'CPX400DP' in instr: 
+    if 'CPX400DP' in instr:                 # add libraries here
         lib_name = 'CPX400DP DC Power Supply'
-    elif 'RF' in instr:
-        lib_name = 'RF supply'
+    elif 'SMW200A' in instr:
+        lib_name = 'SMW200A RF Supply'
+    elif 'MX100TP' in instr:
+        lib_name = 'MX100TP DC Power Supply'
     else:
         lib_name = 'none'                           # if returns 'none' then there is no library available yet for this device
-    
     return lib_name
 
-def updateVoltage(DCSupply, voltage_setting, channel):  # sends voltage setting
+def rf_input_check(values):
+    try:
+        freq = float(values['_PRF_0_0_'])
+        start = float(values['_PRF_0_1_'])
+        stop = float(values['_PRF_0_2_'])
+        step = float(values['_PRF_0_3_'])
+        if freq not in arange(0.1, 30, 0.1):                           # might need to update this depending on the device
+            print('Invalid freq, please use any value from 0.1-30 GHz in steps of 0.1')
+        elif start not in arange(-80, 30, 1):                     # can change this range if needed
+            print('Invalid start RF power, please use any value from -80 - 30 dBm')
+            return 0
+        elif stop not in arange(-80, 30, 1):
+            print('Invalid stop RF power, please use any value from -80 - 30 dBm')
+            return 0 
+        elif stop < start:
+            print('RF stop must be greater than RF start, please change')
+            return 0
+        elif step not in arange(0.1, 2, 0.1):
+            print('invalid Step value, please use any value from 0.1-2.0 dB in steps of 0.1')
+            return 0
+        print('RF parameters OK')
+        return 1
+    except Exception as e:
+        print(type(e))
+        print('-unable to recognise RF parameters please check-')
+
+
+
+
+
+def update_RF_supply(values, sweep_type):           # sweep type used to show which tab is currently in use 
+    freq = float(values['_PRF_0_0_'])               
+    try:
+        if sweep_type == 'power':
+            k = values['_PO1_']                     # get current option menu value
+            index = instr_names.index(k)
+            sel_address = instr_data[index][1]
+            rf_supply =  rm.open_resource(sel_address) # open rf supply instrument
+            rf_supply.write('', freq)
+            rf_supply.write('', freq)
+    except Exception as e:
+        print(type(e))
+        print('-unable to recognise RF parameters please check-')
+        
+    wait(1)
+'''
+def start_POWER_sweep():            # starts power sweep when start button is pressed
+    check whether inputs are valid, e.g start and stop within range
+    break if not, or if ok then advance and assign start as green or add green dot
+    wait(1)
+    step up 0.1 dbm from start to finish
+    wait(0.1)
+    read and wait for outputs
+'''
+
+def update_voltage(dc_supply, voltage_setting, channel):  # sends voltage setting
     try:
         float(voltage_setting)
-        DCSupply.write('V' + str(channel) + ' ' + str(voltage_setting))
+        dc_supply.write('V' + str(channel) + ' ' + str(voltage_setting))
         print('Voltage SET')
     except Exception as e: 
         print(type(e))
         print('voltage setting not a value')
 
-def updateCurrent(DCSupply, current_setting, channel):  # sends current setting
+def update_current(dc_supply, current_setting, channel):  # sends current setting
     try:
         float(current_setting)
-        DCSupply.write('V' + str(channel) + ' ' + str(current_setting))
+        dc_supply.write('V' + str(channel) + ' ' + str(current_setting))
         print('Current SET')
     except Exception as e: 
         print(type(e))
         print('current setting not a value')
 
-def CPX400DP_library():                 # library to determine which options should be disabled if they are not available for this model
-    d = 0
+def update_tab(window, parameter):
+    window['_power_'].update(visible=parameter)
+    window['_freq_'].update(visible=not(parameter))
+
+
+    
+
+
+def CPX400DP_library():                 # library to determine which options/inputs should be disabled in the layout
+    d = 0                               # if they are not available for this model
+
 
 # Define the window's contents
 def make_window(theme):
@@ -101,7 +197,7 @@ def make_window(theme):
       
     headings = ['Freq', 'Voltage', 'Current']
     headings1 = ['Instrument Name', 'VISA Address']
-    rfsupply_columns = ['Samples:', 'Start (dBm):', 'Stop (dBm):', 'Freq (GHz):']
+    rfsupply_columns = ['Freq (GHz):', 'Start (dBm):', 'Stop (dBm):', 'step (dB):']
     dcsupply_columns = ['Voltage (V):', 'Current Lim (A):', 'Other Setting:']
     right_click_menu_def = [[], ['Info', 'Versions', '1', 'Nothing']]
     # layout for input columns
@@ -128,8 +224,8 @@ def make_window(theme):
                     [sg.Text('Power Sweep Setup')],
                     [sg.Text('Select RF Supply (Sweep)', font ='bold'), sg.OptionMenu(values=instr_names, size=(12,1),  k='_PO1_'),           # options menu variables
                     sg.Text('-', font='bold', text_color='red', key='_PLIB1_')],
-                    [sg.Col(rf_column1, size=(600, 25))],
-                    [sg.Col(rf_column2, size=(600, 30))],
+                    [sg.Col(rf_column1, size=(600, 25))],   # rf table texts
+                    [sg.Col(rf_column2, size=(600, 30))],   # rf table inputs
                     # [sg.Text('Samples:', size=(12,1)), sg.Text('Start (dBm):', size=(12,1)), sg.Text('Stop (dBm):', size=(12,1)), sg.Text('Freq (GHz):', size=(12,1))],
                     # [sg.Input(justification='l', size=(14,2), key=('_Pa_' + str(c) + '_')) for c in range(4)],
                     [sg.Text('Select DC Supply 1', pad=((4,4),(10,0)), font ='bold'), sg.OptionMenu(values=instr_names, size=(12,1), pad=((1,1),(10,0)),  k='_PO2_'),           # options menu variables
@@ -182,12 +278,14 @@ def make_window(theme):
 # visa initilization 
 os.add_dll_directory('C:\\Program Files\\Keysight\\IO Libraries Suite\\bin')
 rm = visa.ResourceManager('ktvisa32')
-list = rm.list_resources()          # connected device list, pass ?* for all resources not in INSTR
+instr_list = rm.list_resources()          # connected device list, pass ?* for all resources not in INSTR
 
 def main():
+    global instr_list
     global instr_data
     global instr_names
     window = make_window(sg.theme())
+    print(instr_list)
     # Check connection to all resources in Keysight resource library
     instr_open()
     # Display and interact with the Window using an Event Loop
@@ -238,7 +336,6 @@ def main():
                 window['_TABLE_'].update(values=instr_data)      
             for key in lib_select_keys:
                 window[key].update(values=instr_names)             # update all instrument option menus         
-                window[key].update(values=instr_names)
             print('--------------')
             print(instr_data)
             print('--------------')
@@ -249,45 +346,28 @@ def main():
             window['_INPUTDC1_'].update(visible=False)
 
         elif event == '_R1_':                                           # Power tab enable
-            window['_power_'].update(visible=True)
-            window['_freq_'].update(visible=False)
+            update_tab(window, parameter=True)
         elif event == '_R2_':                                           # Freq tab enable
-            window['_freq_'].update(visible=True)
-            window['_power_'].update(visible=False)
-        elif event == '_OKF_' or event == '_OKP_':                                           # updates all selected libraries
-                                                                            # find current value of option menu in instrument list
-            print('-------- OK --------')                                                               # needs to be done better
-            for i in range(len(lib_select_keys)):
-                print('i=' + str(i))
-                k = values[lib_select_keys[i]]                                        # gives current frequency tab option value
-                print('=> option menu ' + lib_select_keys[i] + ' = ' + k) 
-                try:                                                
-                    index = instr_names.index(k)
-                    print(index)
-                    try:
-                        sel_address = instr_data[index][1]                      # finds address for selected instrument
-                    except Exception as e:
-                        print(type(e))
-                        print('no device found with this GPIB address')
+            update_tab(window, parameter=False)
 
-                    cur_lib_name = lib_select(sel_address)
-                    print(cur_lib_name)
-                    window[lib_change_keys[i]].update(cur_lib_name)                  # shows current active library
-                    if cur_lib_name == 'none':
-                        window[lib_change_keys[i]].update(text_color='red')
-                    else:
-                        window[lib_change_keys[i]].update(text_color='LightGreen')
-                    print('library text updated')
-                except Exception as e:
-                    print(type(e))
-                    print('UPDATE key = ' + lib_select_keys[i] + ' failed')
+        elif event == '_OKF_' or event == '_OKP_':                                           # updates all selected libraries
+        # find current value of option menu in instrument list
+            print('-------- OK --------')                                                               # needs to be done better
+            option_menu_update(window, values, lib_select_keys, lib_change_keys,
+                                instr_names, instr_data)            # this could be done better
 
         if event == '_UPDATEP0_' or event == '_UPDATEP_':                                        # sends values to instrument RF Supply        
             print('-------- Update (P0) --------')
+            if rf_input_check(values) == True:
+                update_RF_supply(values, 'power')
+            else:
+                print('rf input is invalid')
+         
+
         if event == '_UPDATEP1_' or event == '_UPDATEP_':                                        # sends values to instrument DC supply 1
             print('-------- Update (P1) --------')                                                 # updateP1 button needs to be added, if just one instrument
             voltage_setting_1 = values['_PDC1_0_0_']                                               # this entire event can probably be put into a single general function later on
-            voltage_setting_2 = values['_PDC1_1_0_']
+            voltage_setting_2 = values['_PDC1_1_0_']                                               # just generally tidied up
             current_setting_1 = values['_PDC1_0_1_']
             current_setting_2 = values['_PDC1_1_1_']
             print('Power supply 1 channel 1 Voltage: ' + str(voltage_setting_1))
@@ -297,12 +377,12 @@ def main():
                 print(index)
                 sel_address = instr_data[index][1]                                              # finds address for selected instrument
                 DC1 = rm.open_resource(sel_address)
-                updateVoltage(DC1, voltage_setting_1, channel=1)                                           # updates DC supply voltages and currents on this device
-                updateVoltage(DC1, voltage_setting_2, channel=2)                                           
-                updateCurrent(DC1, current_setting_1, channel=1)
-                updateCurrent(DC1, current_setting_2, channel=2)      
+                update_voltage(DC1, voltage_setting_1, channel=1)                                           # updates DC supply voltages and currents on this device
+                update_voltage(DC1, voltage_setting_2, channel=2)                                           
+                update_current(DC1, current_setting_1, channel=1)
+                update_current(DC1, current_setting_2, channel=2)      
             except:
-                print('unable to update instrument')
+                print('-unable to recognise DC1 parameters please check-')
         
 
     window.close()
@@ -312,4 +392,3 @@ def main():
 if __name__ == '__main__':
     sg.theme('DarkBlue4')
     main()
-
